995d328ecaf9fe90dd4d7cbf4b156d37221d015a
Buratishkin <134506806+Buratishkin@users.noreply.github.com>
2024-09-14 14:52:49 +0300

    Add files via upload

---
+++ INF/LabOne/LabOne.py
@@ -0,0 +1,89 @@
+def fib_row(dec_num): 
+
+    arr = [1, 1] 
+
+    while arr[-1] <= dec_num: 
+
+        arr.append(arr[len(arr)-1] + arr[len(arr)-2]) 
+
+   return arr[1 : len(arr) -1] 
+
+ 
+
+def convert_dec_to_fib(dec_num, fib_arr, res):  
+
+    if len(fib_arr) == 0 or dec_num < 0: 
+
+        return 
+
+    el_fib = fib_arr[len(fib)-1] 
+
+    if el_fib > dec_num: 
+
+        res.append("0") 
+
+    else: 
+
+        res.append("1") 
+
+        dec_num -= el_fib  
+
+    fib_arr.pop(len(fib) - 1)  
+
+    convert_dec_to_fib(dec_num, fib_arr, res)  
+
+ 
+
+dec_num = int(input("Введите число в десятичной системе счисления: "))  
+
+ans = []  
+
+fib = fib_row(dec_num)  
+
+convert_dec_to_fib(dec_num, fib, ans)  
+
+print(''.join(ans)) def fib_row(dec_num): 
+
+    arr = [1, 1] 
+
+    while arr[-1] <= dec_num: 
+
+        arr.append(arr[len(arr)-1] + arr[len(arr)-2]) 
+
+   return arr[1 : len(arr) -1] 
+
+ 
+
+def convert_dec_to_fib(dec_num, fib_arr, res):  
+
+    if len(fib_arr) == 0 or dec_num < 0: 
+
+        return 
+
+    el_fib = fib_arr[len(fib)-1] 
+
+    if el_fib > dec_num: 
+
+        res.append("0") 
+
+    else: 
+
+        res.append("1") 
+
+        dec_num -= el_fib  
+
+    fib_arr.pop(len(fib) - 1)  
+
+    convert_dec_to_fib(dec_num, fib_arr, res)  
+
+ 
+
+dec_num = int(input("Введите число в десятичной системе счисления: "))  
+
+ans = []  
+
+fib = fib_row(dec_num)  
+
+convert_dec_to_fib(dec_num, fib, ans)  
+
+print(''.join(ans)) 
