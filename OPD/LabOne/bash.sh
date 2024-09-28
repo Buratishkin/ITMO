011a3541fa20c61aaea7c50d8d4a3d68a269da31
Buratishkin <134506806+Buratishkin@users.noreply.github.com>
2024-09-28 20:41:26 +0300

    Update bash.sh

--- OPD/LabOne/bash.sh
+++ OPD/LabOne/bash.sh
@@ -1,1 +1,80 @@
+#Создание каталога lab0 и переход к нему
+mkdir lab0
+cd lab0
+#Создание каталогов и файлов внутри lab0 + заполнение файлов при помощи vim
+mkdir blastoise4
+cd blastoise4
+  touch venonat
+  vim venonat
+  
+  mkdir finneon
 
+  mkdir doduo
+cd ..  
+
+  
+touch charizard6
+vim charizard6
+
+mkdir clefable6
+cd clefable6
+  mkdir trubbish
+
+  mkdir raichu
+
+  touch eevee
+  vim eevee
+
+  touch azurill
+  vim azurill
+
+  mkdir dusclops
+
+  touch slowpoke
+  vim slowpoke
+cd..
+
+
+touch munchlax9
+vim munchlax9
+
+
+mkdir poliwhirl0
+cd poliwhir0
+  touch piplup
+  vim piplup
+  
+  touch gothita
+  vim gothita
+  
+  touck ekans
+  vim ekans
+cd ..
+
+
+touch rufflet3
+vim rufflet3
+#Задание прав на файлы и каталоги
+chmod 550 blastoise4
+  .
+  .  
+  chmod 570 doduo
+  
+chmod 400 charizard6
+
+chmod 511 clefable6
+  .
+  .
+  chmod 644 eevee
+  .
+  .
+  chmod 660 slowpoke
+
+.
+
+.
+  .
+  chmod 664 gothita
+  .
+
+.  
