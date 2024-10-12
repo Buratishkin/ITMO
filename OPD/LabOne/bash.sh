6f2f61a64d987dcdbd7a8967ec0e4aeb4b953819
Daniil Novikov <134506806+Buratishkin@users.noreply.github.com>
2024-10-12 03:17:44 +0300

    Update bash.sh

--- OPD/LabOne/bash.sh
+++ OPD/LabOne/bash.sh
@@ -64,24 +64,32 @@ chmod 600 poliwhirl0/ekans
 
 chmod 062 rufflet3
 #3
+echo "3.1"
 #cоздать жесткую ссылку для файла rufflet3 с именем lab0/clefable6/slowpokerufflet
 chmod 711 clefable6
 ln clefable6/slowpokerufflet rufflet3
+echo "3.2"
 #cоздать символическую ссылку для файла munchlax9 с именем lab0/poliwhirl0/gothitamunchlax
 ln -s poliwhirl0/gothitamunchlax munchlax9 
+echo "3.3"
 #объеденить содержимое файлов lab0/clefable6/azurill, lab0/clefable6/slowpoke, в новый файл lab0/charizard6_80
 cat clefable6/azurill clefable6/slowpoke > charizard6_80
+echo "3.4"
 #создать символическую ссылку c именем Copy_45 на директорию poliwhirl0 в каталоге lab0
 ln -s poliwhirl0 Copy_45
+echo "3.5"
 #скопировать файл rufflet3 в директорию lab0/blastoise4/doduo
 chmod 750 blastoise4
 chmod 762 rufflet3
 chmod 770 blastoise4/doduo
 cp rufflet3 blastoise4/doduo
+echo "3.6"
 #скопировать содержимое файла charizard6 в новый файл lab0/clefable6/slowpokecharizard
 cp charizard6 clefable6/slowpokecharizard
+echo "3.7"
 #скопировать рекурсивно директорию clefable6 в директорию lab0/blastoise4/finneon
 cp -r clefable6 blastoise4/finneon
+echo "3.8"
 #возвращаю все права доступа обратно
 chmod 550 blastoise4
 chmod 570 blastoise4/doduo
